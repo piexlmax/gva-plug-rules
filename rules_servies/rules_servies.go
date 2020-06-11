ab891160a2750c150d1c482d800187789ddc0ab2
QM303176530 <303176530@qq.com>
2020-06-11 22:30:12 +0800

    更新registerApi 函数

--- rules_servies/rules_servies.go
+++ rules_servies/rules_servies.go
@@ -1,12 +1,14 @@
 package rules_servies
 
 import (
-	"github.com/piexlmax/gva-plug-rules/rules-model"
 	"github.com/jinzhu/gorm"
+	"github.com/piexlmax/gva-plug-rules/rules-model"
 )
 
-func RegistApi(db *gorm.DB,apis []rules_model.GvaPlugApi){
-	for _,v:= range apis{
-		db.Create(&v)
+func RegisterApi(db *gorm.DB, apis []rules_model.GvaPlugApi) {
+	for _, v := range apis {
+		if NeedRegister := db.First(&rules_model.GvaPlugApi{}, "path = ? AND method = ?", v.Path, v.Method).RecordNotFound(); NeedRegister {
+			db.Create(&v)
+		}
 	}
 }
